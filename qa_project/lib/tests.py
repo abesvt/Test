77ee2fed5fcae23fd7012b19dc2f2323dc190bd6
abesvt <mailsvt@gmail.com>
2021-03-19 16:55:24 -0400

    Added setup automation

--- qa_project/lib/tests.py
+++ qa_project/lib/tests.py
@@ -4,22 +4,24 @@ Created on Mar 16, 2021
 @author: AB
 '''
 import unittest
-from lib import startApp
+from lib import startApp, downloadAndInstallApps
 import os
 import logging
 
 
 class Test(unittest.TestCase):
     testpath = "C:\\cribl\\assignment\\"
-    
+    hostIPs = {'Agent':'10.10.10.10','Target1':'10.10.10.10','Target2':'10.10.10.10','Splitter':'10.10.10.10' }
     def setUp(self):
         
         logging.basicConfig(filename=self.testpath + 'test.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
         
-        startApp("target", self.testpath)
-        startApp("splitter", self.testpath)
-        startApp("agent", self.testpath)
-        pass
+        downloadAndInstallApps(self.testpath, self.testpath+"\\NEWtest", self.hostIPs, "user", "pwd")
+        
+        # startApp("target", self.testpath)
+        # startApp("splitter", self.testpath)
+        # startApp("agent", self.testpath)
+        
 
 
     def tearDown(self):
